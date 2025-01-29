e4d5605976bd162e462d00d8dc80accf22ef6b5b
Pasiphique <pasiphiqueoswald@gmail.com>
2025-01-29 11:20:28 -0500

    added pittsburgh neighborhood layer over map

---
+++ map-image-overlay/src/NeighborhoodsLayer.jsx
@@ -0,0 +1,19 @@
+
+import React from "react";
+import { GeoJSON } from "react-leaflet";
+import neighborhoodsData from './assets/neighborhoods_.json'
+
+const NeighborhoodsLayer = () => {
+
+  const geoJsonStyle = {
+    color: "#ff7800",
+    weight: 2,
+    opacity: 1,
+    fillColor: 'grey',
+    fillOpacity: 0.3,
+  };
+
+  return <GeoJSON data={neighborhoodsData} style={geoJsonStyle} />;
+};
+
+export default NeighborhoodsLayer;
--- map-image-overlay/src/main.jsx
+++ map-image-overlay/src/main.jsx
@@ -1,7 +1,8 @@
 
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
-
-createRoot(document.getElementById('root')).render(<App />
+import Home from './Home.jsx'
+import FunFacts from './FunFacts.jsx'
+import Map from './Map.jsx'
+createRoot(document.getElementById('root')).render(<Map />
 )
