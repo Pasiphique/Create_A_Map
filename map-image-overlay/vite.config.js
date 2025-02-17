306c60312146fbe413b00f6847481f73f0d3733d
Pasiphique <pasiphiqueoswald@gmail.com>
2025-02-17 16:51:20 -0500

    change base path

--- map-image-overlay/vite.config.js
+++ map-image-overlay/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"/Create_A_map/",
+  base:"/Create_A_map/map-image-overlay",
 })
